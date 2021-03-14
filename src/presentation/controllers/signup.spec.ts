ac896aad48756de3bcc8f9ea66b834e923ef3cc0
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-14 18:21:37 -0300

    test: checking if husky is configured

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,16 +7,16 @@ describe('Signup Controller', () => {
       body: {
         email: 'any@email.com',
         password: 'any_passowrd',
-        passwordConfirmation: 'any_passowrd'
-      }
+        passwordConfirmation: 'any_passowrd',
+      };
     }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = new SignUpController();
     const httpRequest = {
       body: {
         name: 'any_name',
