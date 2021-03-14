e81a36f355268faed5ff71825479021c4e7a9c75
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-14 18:34:33 -0300

    fix: removing bad code used for libraries testing purpose

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,4 @@
-import {SignUpController} from './signup';
+import { SignUpController } from './signup'
 
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -7,25 +7,25 @@ describe('Signup Controller', () => {
       body: {
         email: 'any@email.com',
         password: 'any_passowrd',
-        passwordConfirmation: 'any_passowrd',
-      };
+        passwordConfirmation: 'any_passowrd'
+      }
     }
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         name: 'any_name',
         password: 'any_passowrd',
-        passwordConfirmation: 'any_passowrd',
+        passwordConfirmation: 'any_passowrd'
       }
     }
-    const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,14 +3,14 @@ export class SignUpController {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: test')
+        body: new Error('Missing param: name')
       }
     }
 
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: teste')
+        body: new Error('Missing param: email')
       }
     }
   }
