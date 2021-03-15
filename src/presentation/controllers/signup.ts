ddf2880088c1491f939ae114d8fc5ab234356289
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-15 20:13:20 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
 
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissinParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissinParamError('name')
-      }
+      return badRequest(new MissinParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissinParamError('email')
-      }
+      return badRequest(new MissinParamError('email'))
     }
   }
 }
