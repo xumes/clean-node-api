6944b9e1faf70decc414cf188c5f0f13f3c9dd51
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-15 21:42:26 -0300

    refactor: move all protocol imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 import { MissinParamError, InvalidParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 interface sutTypes {
   sut: SignUpController
   emailValidatorStub: EmailValidator
