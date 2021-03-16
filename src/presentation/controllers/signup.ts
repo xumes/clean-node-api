03bf196356b6f4f1bf6af8aa9e3833b6de0bd3f8
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-15 21:40:00 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissinParamError } from '../errors/missing-param-error'
+import { MissinParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../errors/invalid-param-error'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   constructor (emailValidator: EmailValidator) {
