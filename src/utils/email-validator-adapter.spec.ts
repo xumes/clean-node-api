30c5a5a8ab194ddebd025461ad286ec6e3dd9bbd
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-19 18:24:22 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
   })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any@email.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any@email.com')
+  })
 })
