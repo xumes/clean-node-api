03fc594ee6375b2768488ac540c9e146e3c46846
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-20 12:20:03 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,13 @@
+import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(accountData.password)
+    return await new Promise(resolve => resolve(null))
+  }
+}
